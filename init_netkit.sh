687518579344f51b843b96e4710fcaa0263aaa98
root <root@localhost.localdomain>
2012-12-06 21:23:24 +0100

    ajout init_netkit

---
+++ init_netkit.sh
@@ -0,0 +1,3 @@
+export NETKIT_HOME=/home/makkron/netkit
+export PATH=/home/makkron/netkit/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/home/makkron/.local/bin:/home/makkron/bin
+export MANPATH=:/home/makkron/netkit/man
